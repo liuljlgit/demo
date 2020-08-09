3a7a24153ea08c3596259f68a949e41544751b28
刘立俊 <645604899@qq.com>
2020-08-09 12:39:34 +0800

    Initial commit

---
+++ jenkins/src/main/java/com/example/jenkins/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.jenkins.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public String tast(){
+        return "test";
+    }
+}
